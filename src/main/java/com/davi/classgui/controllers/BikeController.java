0279a63c6612477001881b3ff8587338fddac850
DaviSFS21 <davisfs2110@gmail.com>
2024-10-02 17:23:20 -0300

    feat: Manipulate the data from the form

--- src/main/java/com/davi/classgui/controllers/BikeController.java
+++ src/main/java/com/davi/classgui/controllers/BikeController.java
@@ -1,4 +1,32 @@
 package com.davi.classgui.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+
 public class BikeController {
+    @FXML
+    ChoiceBox<String> choiceType = new ChoiceBox<>();
+    @FXML
+    Label infoLabel = new Label();
+    @FXML
+    TextField modelText = new TextField();
+    @FXML
+    TextField yearText = new TextField();
+    @FXML
+    Button registerBike;
+
+    @FXML
+    public void initialize() {
+        choiceType.getItems().addAll("MTB", "Speed", "Gravel");
+        choiceType.setValue("MTB");
+    }
+
+    public void showInfo(javafx.event.ActionEvent actionEvent) throws IOException {
+        infoLabel.setText(modelText.getText() + " " + choiceType.getValue() + " " + yearText.getText());
+    }
 }
