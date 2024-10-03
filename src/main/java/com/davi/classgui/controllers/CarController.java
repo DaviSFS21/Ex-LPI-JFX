660b724d28b96b5e3f6cbce949e22c79c2f4921c
DaviSFS21 <davisfs2110@gmail.com>
2024-10-03 17:15:57 -0300

    feat: Finish the car view and controller

---
+++ src/main/java/com/davi/classgui/controllers/CarController.java
@@ -0,0 +1,23 @@
+package com.davi.classgui.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class CarController {
+    @FXML
+    TextField brandText = new TextField();
+    @FXML
+    TextField modelText = new TextField();
+    @FXML
+    TextField yearText = new TextField();
+    @FXML
+    Label infoLabel = new Label();
+    @FXML
+    Button registerCar;
+
+    public void showInfo() {
+        infoLabel.setText(brandText.getText() + " " + modelText.getText() + " " + yearText.getText());
+    }
+}
